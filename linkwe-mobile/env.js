6b4932e2b06db48aeba0aa9f940b2b2e2a72b2bf
姜鹏凯 <848517626@qq.com>
2022-12-15 18:38:27 +0800

    配置调整

--- linkwe-mobile/env.js
+++ linkwe-mobile/env.js
@@ -2,20 +2,14 @@ const envs = {
   development: {
     BASE_URL: '/', // 路由基础路径
     BASE_API: 'http://demo.linkwechat.net/saas-dev-api',
-    APPID: 'ww622fc852f79c3f13', // 企业corpid (仟微科技)
-    AGENTID: '1000080', // 自建应用agentId (LinkWeChat)
   },
   test: {
     BASE_URL: '/mobile/', // 路由基础路径
     BASE_API: 'http://demo.linkwechat.net/linkwechat-api',
-    APPID: 'ww622fc852f79c3f13',
-    AGENTID: '1000080',
   },
   production: {
     BASE_URL: '/mobile/', // 路由基础路径
     BASE_API: 'http://demo.linkwechat.net/linkwechat-api',
-    APPID: 'ww622fc852f79c3f13',
-    AGENTID: '1000080',
   },
 }
 let env = envs[process.env.VUE_APP_ENV]
