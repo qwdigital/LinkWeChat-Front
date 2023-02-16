879d233c0d67efa84da5224fe62285aab3f9bc48
xinla <1570728529@qq.com>
2023-02-17 00:01:20 +0800

    toggle 图标

--- linkwe-pc/src/layout/components/Hamburger/index.vue
+++ linkwe-pc/src/layout/components/Hamburger/index.vue
@@ -1,11 +1,16 @@
 // 显隐侧边栏按钮
 <template>
-  <!-- <i class="el-icon-s-fold" :class="{ 'is-active': $store.state.app.sidebar.opened }" @click="toggleClick"></i> -->
-  <svg-icon
+  <!-- <i class="el-icon-s-fold" :class="{ 'open': $store.state.app.sidebar.opened }" @click="toggle"></i> -->
+  <!-- <svg-icon
     iconClass="nested"
     class="el-icon-s-fold"
-    :class="{ 'is-active': $store.state.app.sidebar.opened }"
-    @click="toggleClick"></svg-icon>
+    :class="{ 'open': $store.state.app.sidebar.opened }"
+    @click="toggle"></svg-icon> -->
+  <div class="toggle" :class="{ open: $store.state.app.sidebar.opened }" @click="toggle">
+    <div class="toggle-line"></div>
+    <div class="toggle-line line-middle"></div>
+    <div class="toggle-line"></div>
+  </div>
 </template>
 
 <script>
@@ -20,7 +25,7 @@ export default {
   //   },
   // },
   methods: {
-    toggleClick() {
+    toggle() {
       this.$store.dispatch('app/toggleSideBar')
     },
   },
@@ -28,10 +33,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.el-icon-s-fold {
-  font-size: 20px;
-  &.is-active {
-    transform: rotate(180deg);
+// .el-icon-s-fold {
+//   font-size: 20px;
+//   &.open {
+//     transform: rotate(180deg);
+//   }
+// }
+.toggle {
+  position: relative;
+  cursor: pointer;
+  .toggle-line {
+    position: relative;
+    left: 0;
+    width: 18px;
+    height: 2px;
+    background: #4e5969;
+    border-radius: 8px;
+    transition: all 0.3s;
+  }
+  .line-middle {
+    position: relative;
+    margin: 4px 0;
+    left: -5px;
+  }
+  &.open {
+    .toggle-line {
+      // transform: rotateY(360deg);
+      left: 3px;
+    }
+    .line-middle {
+      left: 8px;
+    }
   }
 }
 </style>
