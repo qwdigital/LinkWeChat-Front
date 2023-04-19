ed5691be0180b0a9edf389e93db1d4640f123852
姜鹏凯 <848517626@qq.com>
2023-04-19 17:17:19 +0800

    朋友圈更新

--- linkwe-pc/src/views/friendCircle/enterprise.vue
+++ linkwe-pc/src/views/friendCircle/enterprise.vue
@@ -13,7 +13,8 @@
           type="daterange"
           range-separator="——"
           start-placeholder="开始日期"
-          end-placeholder="结束日期"></el-date-picker>
+          end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item>
         <!-- v-hasPermi="['wecom:code:list']" -->
@@ -23,7 +24,7 @@
     </el-form>
     <div class="g-card">
       <div class="mid-action">
-        <el-button type="primary" @click="gotoRoute" style="margin-right: 10px">发表动态</el-button>
+        <el-button type="primary" @click="gotoRoute" style="margin-right: 10px;">发表动态</el-button>
         <div>
           <span class="desc" v-if="lastSyncTime">最近同步：{{ lastSyncTime }}</span>
           <span class="desc" v-else>暂无记录，请手动点击同步</span>
@@ -38,11 +39,13 @@
               :src="row.content"
               fit="fit"
               :preview-src-list="[row.content]"
-              style="width: 100px; height: 100px"></el-image>
+              style="width: 100px; height: 100px;"
+            ></el-image>
             <video
               v-else-if="row.contentType === 'video'"
-              style="width: 100px; height: 100px"
-              :src="row.content"></video>
+              style="width: 100px; height: 100px;"
+              :src="row.content"
+            ></video>
             <div v-else>{{ row.content }}</div>
           </template>
         </el-table-column>
@@ -67,6 +70,22 @@
             <div v-if="row.noAddUserName">
               <TagEllipsis :list="row.noAddUserName.split(',')" />
             </div>
+            <div v-else>全部</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="任务状态" prop="status">
+          <template slot-scope="{ row }">
+            <span>
+              {{
+                row.status == 1
+                  ? '开始创建任务'
+                  : row.status == 2
+                  ? '正在创建任务'
+                  : row.status == 3
+                  ? '创建任务完成'
+                  : ''
+              }}
+            </span>
           </template>
         </el-table-column>
         <el-table-column show-overflow-tooltip prop="createTime" width="170px" label="创建时间"></el-table-column>
@@ -83,22 +102,23 @@
       :visible.sync="dialogVisible"
       title="组织架构"
       :defaultValues="userArray"
-      @success="getSelectUser"></SelectUser>
+      @success="getSelectUser"
+    ></SelectUser>
     <el-dialog title="详情" :visible.sync="detailDialogVisible" width="40%">
       <el-form label-position="right" label-width="100px">
-        <span style="margin-left: 40px; font-size: 16px">内容：</span>
+        <span style="margin-left: 40px; font-size: 16px;">内容：</span>
         <template v-for="(data, index) in detail.otherContent">
           <el-form-item label=" " v-if="data.annexType === 'text'">
             {{ data.other }}
           </el-form-item>
           <el-form-item label=" " v-if="data.annexType === 'video'">
-            <div style="display: inline-block; margin-right: 10px">
-              <video style="width: 200px; height: 200px" :src="data.annexUrl" controls></video>
+            <div style="display: inline-block; margin-right: 10px;">
+              <video style="width: 200px; height: 200px;" :src="data.annexUrl" controls></video>
             </div>
           </el-form-item>
           <el-form-item label=" " v-if="data.annexType === 'link'">
-            <div style="display: inline-block; margin-right: 10px">
-              <span style="width: 100px; height: 100px">{{ data.annexUrl }}</span>
+            <div style="display: inline-block; margin-right: 10px;">
+              <span style="width: 100px; height: 100px;">{{ data.annexUrl }}</span>
             </div>
           </el-form-item>
         </template>
@@ -107,10 +127,11 @@
             <div v-for="(data, index) in detail.otherContent">
               <el-image
                 class="item"
-                style="width: 100px; height: 100px"
+                style="width: 100px; height: 100px;"
                 :preview-src-list="[data.annexUrl]"
                 v-if="data.annexType === 'image'"
-                :src="data.annexUrl"></el-image>
+                :src="data.annexUrl"
+              ></el-image>
             </div>
           </div>
         </el-form-item>
@@ -129,166 +150,166 @@
   </div>
 </template>
 <script>
-import moment from 'moment'
-import { getEnterpriceList, syncHMoments, getDetail } from '@/api/circle'
-export default {
-  name: 'enterprise',
-  components: {},
-  data() {
-    return {
-      disable: false,
-      value1: [],
-      dialogVisible: false,
-      userArray: [],
-      query: {
-        pageSize: 10,
-        pageNum: 1,
-        beginTime: '',
-        endTime: '',
-        creator: '',
-        type: 0,
+  import moment from 'moment'
+  import { getEnterpriceList, syncHMoments, getDetail } from '@/api/circle'
+  export default {
+    name: 'enterprise',
+    components: {},
+    data() {
+      return {
+        disable: false,
+        value1: [],
+        dialogVisible: false,
+        userArray: [],
+        query: {
+          pageSize: 10,
+          pageNum: 1,
+          beginTime: '',
+          endTime: '',
+          creator: '',
+          type: 0
+        },
+        loading: false,
+        tableData: [],
+        total: 0,
+        lastSyncTime: '',
+        detailDialogVisible: false,
+        detail: {}
+      }
+    },
+    methods: {
+      dealType(type) {
+        switch (type) {
+          case 4:
+            return '文本'
+            break
+          case 0:
+            return '图片'
+            break
+          case 9:
+            return '图文'
+            break
+          case 11:
+            return '小程序'
+            break
+          case 12:
+            return '文章'
+            break
+          case 2:
+            return '视频'
+            break
+          case 3:
+            return '文件'
+            break
+          case 5:
+            return '海报'
+            break
+          default:
+            return '文本'
+            break
+        }
       },
-      loading: false,
-      tableData: [],
-      total: 0,
-      lastSyncTime: '',
-      detailDialogVisible: false,
-      detail: {},
-    }
-  },
-  methods: {
-    dealType(type) {
-      switch (type) {
-        case 4:
-          return '文本'
-          break
-        case 0:
-          return '图片'
-          break
-        case 9:
-          return '图文'
-          break
-        case 11:
-          return '小程序'
-          break
-        case 12:
-          return '文章'
-          break
-        case 2:
-          return '视频'
-          break
-        case 3:
-          return '文件'
-          break
-        case 5:
-          return '海报'
-          break
-        default:
-          return '文本'
-          break
-      }
-    },
-    gotoRoute() {
-      this.$router.push({
-        path: '/customerMaintain/friendCircle/publish',
-      })
-    },
-    setTimeChange(e) {
-      if (e) {
-        this.query.beginTime = moment(e[0]).format('YYYY-MM-DD')
-        this.query.endTime = moment(e[1]).format('YYYY-MM-DD')
-      } else {
-        this.query.beginTime = ''
-        this.query.endTime = ''
-      }
-    },
-    detailFn(id) {
-      this.detailDialogVisible = true
-      getDetail(id).then((dd) => {
-        if (dd.code === 200) {
-          this.detail = dd.data
-        }
-      })
-    },
-    syncFn() {
-      syncHMoments({ filterType: 0 }).then((res) => {
-        if (res.code === 200) {
-          this.msgSuccess(res.msg)
-          this.getList()
-        }
-      })
-    },
-    getSelectUser(data) {
-      this.userArray = data
-      this.query.creator = this.userArray
-        .map(function (obj, index) {
-          return obj.name
+      gotoRoute() {
+        this.$router.push({
+          path: '/customerMaintain/friendCircle/publish'
         })
-        .join(',')
-    },
-    resetQuery() {
-      ;(this.query = {
-        pageSize: 10,
-        pageNum: 1,
-        beginTime: '',
-        endTime: '',
-        creator: '',
-        type: 0,
-      }),
-        (this.userArray = [])
-      this.value1 = []
-      this.getList()
-    },
-    getList(type) {
-      type && (this.query.pageNum = type)
-      this.loading = true
-      getEnterpriceList(this.query)
-        .then((res) => {
-          this.tableData = res.rows
-          this.lastSyncTime = res.lastSyncTime
-          this.setTimeDiff()
-          this.total = Number(res.total)
-          this.loading = false
-        })
-        .catch(() => {
-          this.loading = false
-        })
-    },
-    setTimeDiff() {
-      if (this.lastSyncTime) {
-        let date1 = moment(this.lastSyncTime)
-        let date2 = moment()
-        let date3 = date2.diff(date1, 'minute')
-        const h = Math.floor(date3 / 60)
-        console.log(h)
-        if (h >= 2) {
-          this.disable = false
+      },
+      setTimeChange(e) {
+        if (e) {
+          this.query.beginTime = moment(e[0]).format('YYYY-MM-DD')
+          this.query.endTime = moment(e[1]).format('YYYY-MM-DD')
         } else {
-          this.disable = true
+          this.query.beginTime = ''
+          this.query.endTime = ''
+        }
+      },
+      detailFn(id) {
+        this.detailDialogVisible = true
+        getDetail(id).then((dd) => {
+          if (dd.code === 200) {
+            this.detail = dd.data
+          }
+        })
+      },
+      syncFn() {
+        syncHMoments({ filterType: 0 }).then((res) => {
+          if (res.code === 200) {
+            this.msgSuccess(res.msg)
+            this.getList()
+          }
+        })
+      },
+      getSelectUser(data) {
+        this.userArray = data
+        this.query.creator = this.userArray
+          .map(function (obj, index) {
+            return obj.name
+          })
+          .join(',')
+      },
+      resetQuery() {
+        ;(this.query = {
+          pageSize: 10,
+          pageNum: 1,
+          beginTime: '',
+          endTime: '',
+          creator: '',
+          type: 0
+        }),
+          (this.userArray = [])
+        this.value1 = []
+        this.getList()
+      },
+      getList(type) {
+        type && (this.query.pageNum = type)
+        this.loading = true
+        getEnterpriceList(this.query)
+          .then((res) => {
+            this.tableData = res.rows
+            this.lastSyncTime = res.lastSyncTime
+            this.setTimeDiff()
+            this.total = Number(res.total)
+            this.loading = false
+          })
+          .catch(() => {
+            this.loading = false
+          })
+      },
+      setTimeDiff() {
+        if (this.lastSyncTime) {
+          let date1 = moment(this.lastSyncTime)
+          let date2 = moment()
+          let date3 = date2.diff(date1, 'minute')
+          const h = Math.floor(date3 / 60)
+          console.log(h)
+          if (h >= 2) {
+            this.disable = false
+          } else {
+            this.disable = true
+          }
         }
       }
     },
-  },
-  mounted() {},
-  created() {
-    this.value1[1] = moment(new Date()).format('YYYY-MM-DD')
-    this.value1[0] = moment(new Date()).subtract(1, 'months').format('YYYY-MM-DD')
-    this.query.beginTime = moment(this.value1[0]).format('YYYY-MM-DD')
-    this.query.endTime = moment(this.value1[1]).format('YYYY-MM-DD')
-    this.getList()
-  },
-}
+    mounted() {},
+    created() {
+      this.value1[1] = moment(new Date()).format('YYYY-MM-DD')
+      this.value1[0] = moment(new Date()).subtract(1, 'months').format('YYYY-MM-DD')
+      this.query.beginTime = moment(this.value1[0]).format('YYYY-MM-DD')
+      this.query.endTime = moment(this.value1[1]).format('YYYY-MM-DD')
+      this.getList()
+    }
+  }
 </script>
 <style lang="scss" scoped>
-.img_list {
-  display: flex;
-  flex-wrap: wrap;
-  width: 330px;
-  .item {
-    flex: 1;
-    width: 110px;
-    height: 100px;
-    margin-right: 5px;
+  .img_list {
+    display: flex;
+    flex-wrap: wrap;
+    width: 330px;
+    .item {
+      flex: 1;
+      width: 110px;
+      height: 100px;
+      margin-right: 5px;
+    }
   }
-}
 </style>
