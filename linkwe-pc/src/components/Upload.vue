8aa3e39784142da54599d897db6dfc755ad27b0c
xinla <1570728529@qq.com>
2023-05-22 14:35:57 +0800

    图片上传宽高限制1024

--- linkwe-pc/src/components/Upload.vue
+++ linkwe-pc/src/components/Upload.vue
@@ -44,7 +44,7 @@ export default {
     // 图片的宽高像素限制 [width(number), height(number)],默认null不限制
     maxImgPx: {
       type: Array,
-      default: null, // () => [100, 100]
+      default: [1024, 1024], // () => [100, 100]
     },
     // 允许上传的文件格式后缀名 eg:["jpg", "png"]，['*']为不限制，各类型有默认限制 参见: formatDefault
     format: {
