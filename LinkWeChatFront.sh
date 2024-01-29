710e1e885d1cead08ab237c315d4c52618a7788a
xinla <1570728529@qq.com>
2024-01-29 15:44:27 +0800

    添加自动化开发构建部署shell脚本

---
+++ LinkWeChatFront.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+
+echo 'LinkWeChatFront.sh...'
+
+cd $(dirname $0)
+
+if ! command -v git &> /dev/null; then
+    echo 请安装 Git
+    exit
+fi
+
+if [ -d "$(pwd)/link-we-chat-front" ]; then
+    echo 拉取项目...
+    git pull
+    echo 拉取完毕
+else
+    echo 拉取项目...
+    git clone https://gitee.com/LinkWeChat/link-we-chat-front.git
+    echo 拉取完毕
+fi
+
+if ! command -v node &> /dev/null; then
+    echo 请安装 Node.js
+    exit
+fi
+
+cnpm() {
+    if ! command -v cnpm &> /dev/null; then
+        echo 安装cnpm...
+        npm install -g cnpm --registry=https://registry.npmmirror.com
+        echo cnpm已安装
+    fi
+}
+
+# 打包pc端
+cd vue3-lw-pc
+
+project=$(basename "$(dirname) $(pwd)")
+
+echo 当前项目${project}
+
+if [ ! -d "$(pwd)/node_modules" ]; then
+    cnpm
+    echo 安装依赖...
+    cnpm i
+    echo 依赖已安装
+fi
+
+echo 打包 ${project} 生产环境...
+npm run build
+echo 打包完成
+
+# 打包vue2移动端
+cd ../linkwe-mobile
+
+project=$(basename "$(dirname) $(pwd)")
+
+echo 当前项目${project}
+
+if [ ! -d "$(pwd)/node_modules" ]; then
+    cnpm
+    echo 安装依赖...
+    cnpm i
+    echo 依赖已安装
+fi
+
+echo 打包 ${project} 生产环境...
+npm run build
+echo 打包完成
+
+# 打包vue3移动端
+cd ../vue3-lw-mobile
+
+project=$(basename "$(dirname) $(pwd)")
+
+echo 当前项目${project}
+
+if [ ! -d "$(pwd)/node_modules" ]; then
+    cnpm
+    echo 安装依赖...
+    cnpm i
+    echo 依赖已安装
+fi
+
+echo 打包 ${project} 生产环境...
+npm run build
+echo 打包完成
+
+
---
+++ vue3-lw-pc/bin/dev.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+echo 打包开发环境
+
+cd $(dirname $0)
+
+if ! command -v git &> /dev/null; then
+    echo 请安装 Git
+    exit
+fi
+
+if ! command -v node &> /dev/null; then
+    echo 请安装 Node.js
+    exit
+fi
+
+cd ../../
+
+echo 拉取项目...
+git pull
+echo 拉取完毕
+
+cd vue3-lw-pc
+
+if [ ! -d "$(pwd)/node_modules" ]; then
+    if ! command -v cnpm &> /dev/null; then
+        echo 安装cnpm...
+        npm install -g cnpm --registry=https://registry.npmmirror.com
+        echo cnpm已安装
+    fi
+    echo 安装依赖...
+    cnpm i
+    echo 依赖已安装
+fi
+
+npm run dev
